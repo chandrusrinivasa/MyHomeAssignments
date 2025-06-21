714a388c6d8678cf658ff642b1439ad1d0b80132
chandrusrinivasa <106592003+chandrusrinivasa@users.noreply.github.com>
2025-06-21 21:14:38 +0530

    Add files via upload
    Home Assignment <2> Understanding Data Types and Variables in Java

---
+++ Homeassignments/Chrome.java
@@ -0,0 +1,22 @@
+package Homeassignments;
+
+public class Chrome {
+
+	public static void main(String[] args) {
+		float Chromeversion = 91.0f;
+		String Chromedeveloperversion = "Google";
+		Boolean  Chromeisbeta = false;
+		int ChromerealseYear = 1998;
+		char ShortcutKey ='C';
+		
+		System.out.println(
+				        "Chrome Version               :   "+Chromeversion + "\n"
+				       +"Chrome Developer Version     :   "+Chromedeveloperversion +"\n"
+				       +"Chrome Beta Version          :   "+Chromeisbeta+"\n"
+				       +"Chrome Lauch year            :   "+ChromerealseYear+"\n"
+				       +"Short Cut Key for Chrome     :   "+ShortcutKey);
+		
+
+	}
+
+}
