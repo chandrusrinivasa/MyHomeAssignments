d6d84cb31d86d61b036cbd8a9d32490e761b6d84
chandrusrinivasa <106592003+chandrusrinivasa@users.noreply.github.com>
2025-06-27 08:46:38 +0530

    Add files via upload
    Day2_HomeAssignment

---
+++ Homeassignment_day2/CheckNumberIsPositive.java
@@ -0,0 +1,16 @@
+package Homeassignment_day2;
+
+public class CheckNumberIsPositive {
+
+	public static void main(String[] args) {
+		int chandru =10;
+		if (chandru>0) {
+			System.out.println("Positive number");
+		}
+		else {
+			System.out.println("Negative number");
+		}
+
+	}
+
+}
